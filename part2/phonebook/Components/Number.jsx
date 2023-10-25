5875faa4c785c053f915898d43bdd20fa060b891
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2023-10-25 12:01:34 -0700

    exercise 2.6

---
+++ part2/phonebook/Components/Number.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Number = ({ num }) => {
+  return <p>{num.name}</p>;
+};
+
+export default Number;
---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Number from "../Components/Number";
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addNumber = (e) => {
+    e.preventDefault();
+    const noteObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(noteObject));
+    setNewName("");
+  };
+
+  const handleChange = (e) => {
+    setNewName(e.target.value);
+  };
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNumber}>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((ele) => (
+          <Number key={ele.name} num={ele} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
