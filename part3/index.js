63bc4198dd94424d723e5160e8e686e3125e0418
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-01-04 20:26:02 -0800

    exercise 3.6

--- part3/index.js
+++ part3/index.js
@@ -54,7 +54,18 @@ app.delete("/api/persons/:id", (req, res) => {
 
 app.post("/api/persons/", (req, res) => {
   const newPerson = req.body;
-  console.log(req.body);
+  if (!newPerson.name || !newPerson.number) {
+    return res.status(400).json({
+      error: "The name or number is missing",
+    });
+  } else if (
+    persons.find((p) => p.name.toLowerCase() === newPerson.name.toLowerCase())
+  ) {
+    return res.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
   const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
   newPerson.id = maxId + 1;
   persons = persons.concat(newPerson);
