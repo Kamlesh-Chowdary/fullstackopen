47cc66c9f40d2d583e371768fc745e3fc5b636fb
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-01-08 19:32:58 -0800

    retrieving data from mongoDB

--- part3/mongo.js
+++ part3/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const [, password, name, number] = process.argv;
+const [, , password, name, number] = process.argv;
 
 const URI = `mongodb+srv://kamlesh:${password}@cluster0.ovhquw0.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -15,6 +15,17 @@ const People = mongoose.model("Person", personSchema);
 
 const Person = new People({ name, number });
 
+if (process.argv.length < 3) {
+  console.log("phonebook:");
+  Person.find({}).then((res) => {
+    res.forEach((p) => {
+      console.log(p.name, p.number);
+    });
+    mongoose.connection().close();
+  });
+}
+
 Person.save().then((result) => {
+  console.log(`Added ${name}'s number ${number} to phonebook`);
   mongoose.connection().close();
 });
