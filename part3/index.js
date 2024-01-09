77327ccb446352b220dc2b7f754964de67a87e93
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-01-09 14:47:30 -0800

    add .env file

--- part3/index.js
+++ part3/index.js
@@ -82,7 +82,9 @@ const unknownEndPoint = (req, res) => {
 };
 
 app.use(unknownEndPoint);
+
 const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
