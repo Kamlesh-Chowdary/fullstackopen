55283da92bd10792d1949701427f476a414e55a9
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-01-10 10:28:28 -0800

    removed person

--- part3/index.js
+++ part3/index.js
@@ -13,29 +13,6 @@ app.use(
   morgan(":method :url  :status :res[content-length] - :response-time ms :body")
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/api/persons", (req, res) => {
   Person.find({}).then((initialNumbers) => {
     res.json(initialNumbers);
@@ -48,14 +25,12 @@ app.get("/info", (req, res) => {
   res.send(data);
 });
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
   Person.findById(req.params.id)
     .then((number) => {
       res.send(number);
     })
-    .catch((error) => {
-      res.send("Error : Wrong ID");
-    });
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (req, res, next) => {
@@ -87,12 +62,6 @@ app.post("/api/persons/", (req, res) => {
   number.save().then((newNumber) => {
     res.status(200).send(newNumber);
   });
-  //  else if (
-  //   persons.find((p) => p.name.toLowerCase() === newPerson.name.toLowerCase())
-  // ) {
-  //   return res.status(400).json({
-  //     error: "name must be unique",
-  //   });
 });
 const unknownEndPoint = (req, res) => {
   res.status(404).send({ error: "Unknown End Point" });
@@ -100,6 +69,14 @@ const unknownEndPoint = (req, res) => {
 
 app.use(unknownEndPoint);
 
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+app.use(errorHandler);
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
