4edb2aa6a8a06d3b4a6289190f03d3a9ac8a00a2
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-01-17 13:48:14 -0800

    add controller (route paths)

--- part4/index.js
+++ part4/index.js
@@ -19,20 +19,6 @@ mongoose.connect(config.MONGODB_URI);
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
