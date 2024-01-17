e6145e2abb7c3a95758b91ae1ef5fd617f8f2054
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-01-17 14:09:27 -0800

    exercise 4.2

--- part4/index.js
+++ part4/index.js
@@ -1,12 +1,7 @@
-const app = express();
-const cors = require("cors");
-const logger = require("./utils/logger");
+const app = require("./app");
 const config = require("./utils/config");
-const blogsRouter = require("./controllers/blogs");
+const logger = require("./utils/logger");
 
-app.use(cors());
-app.use(express.json());
-app.use("/api/blogs", blogsRouter);
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
--- part4/models/blog.js
+++ part4/models/blog.js
@@ -1,6 +1,5 @@
-const config = require("../utils/config");
 const mongoose = require("mongoose");
-mongoose.connect(config.MONGODB_URI);
+
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
