9f51c5da9f81e05b654992d39fc99e2891a24f55
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-01-17 11:47:33 -0800

    added logger file

--- part4/index.js
+++ part4/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-
+const logger = require("./utils/logger");
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
@@ -33,7 +33,7 @@ app.post("/api/blogs", (request, response) => {
   });
 });
 
-const PORT = 3003;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  logger.info(`Server running on port ${PORT}`);
 });
