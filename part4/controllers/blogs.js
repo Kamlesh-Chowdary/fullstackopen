62d9c55c012112ea865701b5057f21a50df315a7
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-01-17 13:57:34 -0800

    add models (database)

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,13 +1,17 @@
-app.get("/api/blogs", (request, response) => {
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+
+blogsRouter.get("/", (request, response) => {
   Blog.find({}).then((blogs) => {
     response.json(blogs);
   });
 });
 
-app.post("/api/blogs", (request, response) => {
+app.post("/", (request, response) => {
   const blog = new Blog(request.body);
 
   blog.save().then((result) => {
     response.status(201).json(result);
   });
 });
+module.exports = blogsRouter;
--- part4/index.js
+++ part4/index.js
@@ -1,24 +1,12 @@
-const express = require("express");
 const app = express();
 const cors = require("cors");
-const mongoose = require("mongoose");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
-require("dotenv").config();
-
-mongoose.connect(config.MONGODB_URI);
+const blogsRouter = require("./controllers/blogs");
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/blogs", blogsRouter);
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
 });
