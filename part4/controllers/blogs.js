701e7997b8aea3e532f41e42cb9fa2fb036e7f61
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-01-21 19:42:13 -0800

    exercise 4.20

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -2,13 +2,6 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
-const getTokenFrom = (request) => {
-  const authorization = request.get("authorization");
-  if (authorization && authorization.startsWith("Bearer ")) {
-    return authorization.replace("Bearer ", "");
-  }
-  return null;
-};
 blogsRouter.get("/", async (request, response) => {
   const blogPost = await Blog.find({}).populate("user", {
     username: 1,
@@ -20,15 +13,10 @@ blogsRouter.get("/", async (request, response) => {
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
 
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
-  if (!decodedToken.id) {
-    request.user = null;
-  } else {
-    request.user = await User.findById(decodedToken.id);
+  const user = request.user;
+  if (!user) {
+    return response.status(401).json({ error: "token missing or invalid" });
   }
-
-  const user = await User.findById(decodedToken.id);
-
   const newBlog = new Blog({
     title: body.title,
     author: body.author,
--- part4/utils/middleware.js
+++ part4/utils/middleware.js
@@ -1,5 +1,6 @@
 const logger = require("./logger");
-
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 const requestLogger = (request, response, next) => {
   logger.info("Method:", request.method);
   logger.info("Path:  ", request.path);
@@ -19,13 +20,41 @@ const errorHandler = (req, res, next) => {
     return response.status(400).send({ error: "malformatted id" });
   } else if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message });
+  } else if (error.name === "TokenExpiredError") {
+    return response.status(401).json({
+      error: "token expired",
+    });
   }
 
   next(error);
 };
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    req.token = authorization.replace("Bearer ", "");
+    return next();
+  }
+  req.token = null;
+  return next();
+};
 
+const userExtractor = async (req, res, next) => {
+  if (!req.token) {
+    req.user = null;
+  } else {
+    const decodedToken = jwt.verify(req.token, process.env.SECRET);
+    if (!decodedToken.id) {
+      req.user = user;
+    } else {
+      req.user = await User.findById(decodedToken.id);
+    }
+  }
+  next();
+};
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
+  tokenExtractor,
+  userExtractor,
 };
