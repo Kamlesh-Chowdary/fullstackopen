5ee23978b40e4f30229c8eceab3fc02d6aaebb08
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-01-25 22:29:10 -0800

    view button

--- part5/src/components/Blog.jsx
+++ part5/src/components/Blog.jsx
@@ -1,7 +1,17 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {"===>"} {blog.author}
-  </div>
-);
+const Blog = ({ blog, handleView }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
 
+  return (
+    <div style={blogStyle}>
+      {blog.title} {"===>"} {blog.author}
+      <button onClick={handleView}> view</button>
+    </div>
+  );
+};
 export default Blog;
