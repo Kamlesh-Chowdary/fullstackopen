f0c1f18ae70564d2cd5c54d63f18ca85eee75550
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-01-25 20:41:54 -0800

    exercise 5.5

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -3,6 +3,67 @@ import Blog from "./components/Blog";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
 import Notification from "./components/Notification";
+// import AddBlog from "./components/AddBlog";
+
+const AddBlog = ({
+  addNewBlog,
+  title,
+  setTitle,
+  author,
+  setAuthor,
+  url,
+  setUrl,
+  likes,
+  setLikes,
+  toggleCreate,
+}) => {
+  return (
+    <div>
+      <h2>Create new blog</h2>
+      <form>
+        <div>
+          title :
+          <input
+            type="text"
+            value={title}
+            id="title"
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author :
+          <input
+            type="text"
+            value={author}
+            id="author"
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url :
+          <input
+            type="text"
+            value={url}
+            id="url"
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <div>
+          likes :
+          <input
+            type="text"
+            value={likes}
+            id="likes"
+            onChange={({ target }) => setLikes(target.value)}
+          />
+        </div>
+        <button onClick={addNewBlog}>create</button>
+        <button onClick={toggleCreate}>cancel</button>
+      </form>
+    </div>
+  );
+};
+// export default AddBLog
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [user, setUser] = useState(null);
@@ -13,7 +74,7 @@ const App = () => {
   const [url, setUrl] = useState("");
   const [likes, setLikes] = useState("");
   const [notification, setNotification] = useState({ message: null });
-
+  const [createBlog, setCreateBlog] = useState(false);
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
@@ -35,7 +96,12 @@ const App = () => {
       setNotification({ message: null });
     }, 3000);
   };
-
+  const clearBlogForm = () => {
+    setTitle("");
+    setAuthor("");
+    setUrl("");
+    setLikes("");
+  };
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
@@ -93,68 +159,54 @@ const App = () => {
       url: url,
       likes: likes,
     };
-    const result = await blogService.create(newBlog);
-    if (result) {
-      notifyWith(`A new blog ${title} by ${author} is added.`);
-      setTitle("");
-      setAuthor("");
-      setUrl("");
-      setLikes("");
-      setBlogs(blogs.concat(result));
+    try {
+      const result = await blogService.create(newBlog);
+
+      if (result && result.title) {
+        notifyWith(`A new blog ${title} by ${author} is added.`);
+        clearBlogForm();
+        setBlogs((prevBlogs) => [...prevBlogs, result]);
+        toggleCreate();
+      }
+    } catch (error) {
+      notifyWith(error.response?.data.error || "An error occurred", "error");
     }
   };
+  const toggleCreate = () => {
+    setCreateBlog(!createBlog);
+    clearBlogForm();
+  };
 
+  const createBlogForm = () => {
+    if (!createBlog) {
+      return <button onClick={toggleCreate}>new blog</button>;
+    }
+    return (
+      <AddBlog
+        addNewBlog={addNewBlog}
+        title={title}
+        setTitle={setTitle}
+        author={author}
+        setAuthor={setAuthor}
+        url={url}
+        setUrl={setUrl}
+        likes={likes}
+        setLikes={setLikes}
+        toggleCreate={toggleCreate}
+      />
+    );
+  };
   return (
     <div>
       <h2>blogs</h2>
       <Notification message={notification} />
-      <p>{user.name} Logged in</p>
-      <button type="submit" onClick={handleLogout}>
-        logout
-      </button>
-
-      <div>
-        <h2>Create new blog</h2>
-        <form onSubmit={addNewBlog}>
-          <div>
-            title :
-            <input
-              type="text"
-              value={title}
-              id="title"
-              onChange={({ target }) => setTitle(target.value)}
-            />
-          </div>
-          <div>
-            author :
-            <input
-              type="text"
-              value={author}
-              id="author"
-              onChange={({ target }) => setAuthor(target.value)}
-            />
-          </div>
-          <div>
-            url :
-            <input
-              type="text"
-              value={url}
-              id="url"
-              onChange={({ target }) => setUrl(target.value)}
-            />
-          </div>
-          <div>
-            likes :
-            <input
-              type="text"
-              value={likes}
-              id="likes"
-              onChange={({ target }) => setLikes(target.value)}
-            />
-          </div>
-          <button type="submit">create</button>
-        </form>
-      </div>
+      <p>
+        {user.name} Logged in{" "}
+        <button type="submit" onClick={handleLogout}>
+          logout
+        </button>
+      </p>
+      {createBlogForm()}
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
