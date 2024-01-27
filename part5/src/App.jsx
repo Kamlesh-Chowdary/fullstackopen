976c807ab402317af6df696aa93434e817eb97e1
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-01-26 19:28:10 -0800

    exercise 5.10

--- part5/src/App.jsx
+++ part5/src/App.jsx
@@ -14,7 +14,10 @@ const App = () => {
   const [notification, setNotification] = useState({ message: null });
 
   useEffect(() => {
-    blogService.getAll().then((blogs) => setBlogs(blogs));
+    blogService.getAll().then((blogs) => {
+      blogs.sort((a, b) => b.likes - a.likes);
+      setBlogs(blogs);
+    });
   }, []);
 
   useEffect(() => {
