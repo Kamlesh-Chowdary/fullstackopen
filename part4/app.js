9ef884d0406d46cbca0af6faeee37ae5dae07301
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-01-28 11:14:35 -0800

    exercise 5.17

--- part4/app.js
+++ part4/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+const logger = require("./utils/logger");
+
 const config = require("./utils/config");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
@@ -20,6 +22,11 @@ app.use(middleware.tokenExtractor);
 app.use("/api/blogs", middleware.userExtractor, blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+  logger.info(`Server running on port ${config.PORT}`);
+}
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
